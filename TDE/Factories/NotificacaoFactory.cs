286f4864d42bdb7f9b27e4baca52f395e2e6b7fd
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-15 14:37:54 -0300

    feat: implementar criação de notificações

---
+++ TDE/Factories/NotificacaoFactory.cs
@@ -0,0 +1,20 @@
+using Interfaces;
+using Models;
+
+public class NotificacaoFactory
+{
+    public static INotificacao Criar(string tipo)
+    {
+        switch (EmitTypeInformation.ToLower())
+        {
+            case "email":
+                return new Email();
+            case "sms":
+                return SMS();
+            case "whatsapp":
+                return new WhatsaApp();
+            default:
+                throw new System.Exception("Tipo de notificação inválido");
+        }
+    }
+}
