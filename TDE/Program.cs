2a779bf8babbfde55f4bf59d5feb087efdfd4300
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-15 14:54:06 -0300

    feat(Program):criar entry point do sistema de notificações

--- TDE/Program.cs
+++ TDE/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Franciele!");
+﻿using System;
+using Services;
+using Singleton;
+
+class Program{
+
+    static void Main(string[] args){
+    
+        var config = ConfiguracaoSistema.GetInstancia();
+        console.WriteLine($"Sistema: {config.NomeSistema} - Versão {config.Versao}\n");
+
+        var service = new NotificacaoService();
+
+        service.EnviarNotificacao("email");
+        service.EnviarNotificacao("sms");
+        service.EnviarNotificacao("whatsapp");
+    }
+}
